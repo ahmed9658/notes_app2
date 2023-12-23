8e66dcdf6e168a94f11a5ff2c4f2a4ac7522bd7f
zeed <zeedelzeedi@outlook.com>
2023-12-23 14:27:46 +0200

    create app

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class notes_app extends StatelessWidget {
+  const notes_app({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+
+    );
+  }
+}
+
+void main(){
+  runApp(notes_app());
+}
